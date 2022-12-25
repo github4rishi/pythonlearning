d8497aa2ebcaccd563a1834becd1a7389b5315fa
Rishi Kapoor <in.rishi@gmail.com>
2022-12-25 07:02:36 +0530

    loops understanding

---
+++ exercise_files/loops.py
@@ -0,0 +1,14 @@
+# Simple program to understand for loop
+print('for loop')
+a = [1,2,3,4,5]
+for n in a:
+    print(n)
+
+
+# Simple program to understand while loop
+
+print('while loop')
+a = 0 
+while a < 5:
+    print(a)
+    a = a + 1
