de743da33c93723c000b9cba7c11af5ec09ead2a
Rishi Kapoor <in.rishi@gmail.com>
2023-01-18 11:28:25 +0530

    input and output

---
+++ exercise_files/addition_cal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# addition cal
+a=4
+b=5
+result=a+b
+print(f'Addition of {a} and {b} is: {result}')
+
+
+# prompt for input and then add the numbers
+a=eval(input("Enter value of a "))
+b=eval(input("Enter value of b "))
+print(f'The value of a is: {a} and value of b is: {b}, addition = {a + b}')
