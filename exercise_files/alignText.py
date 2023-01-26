116951b0860dd60e87054c57f186718e894b8ccb
Rishi Kapoor <in.rishi@gmail.com>
2023-01-26 07:03:59 +0000

    align text

---
+++ exercise_files/alignText.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Print given string in the centre
+# Author: Rishi Kapoor
+# Date: 26.01.2023
+
+string=input("Enter the string: ")
+print(string.center(80))
+print(string.ljust(80))
+print(string.rjust(80))
