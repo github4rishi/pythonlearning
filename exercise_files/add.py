b6209f8183eb67040584e2cf89564d1a2f2dba0c
Rishi Kapoor <in.rishi@gmail.com>
2023-01-26 07:17:13 +0000

    practice files

--- exercise_files/add.py
+++ exercise_files/add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # Program to add two numbers
 
-n1 = 10
-n2 = 20
+n1=eval(input("Enter 1st number: "))
+n2=eval(input("Enter 2nd number: "))
 
 print(f"value of n1 is {n1} \nvalue of n2 is {n2}\nsum is {n1 + n2}")
 
