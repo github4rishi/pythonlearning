01dc4fef8a2907c7689ae43bc5d9e5a08770e72b
Rishi Kapoor <in.rishi@gmail.com>
2023-01-26 07:23:55 +0000

    changes in alignText script

--- exercise_files/add.py
+++ exercise_files/add.py
@@ -5,4 +5,3 @@ n1=eval(input("Enter 1st number: "))
 n2=eval(input("Enter 2nd number: "))
 
 print(f"value of n1 is {n1} \nvalue of n2 is {n2}\nsum is {n1 + n2}")
-
--- exercise_files/alignText.py
+++ exercise_files/alignText.py
@@ -2,8 +2,10 @@
 # Print given string in the centre
 # Author: Rishi Kapoor
 # Date: 26.01.2023
+import os
+term_width=os.get_terminal_size().columns
 
 string=input("Enter the string: ")
-print(string.center(80))
-print(string.ljust(80))
-print(string.rjust(80))
+print(string.center(term_width))
+print(string.ljust(term_width))
+print(string.rjust(term_width))
